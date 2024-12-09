87184cd2b1e3e99b1e50cf58bddde84aa2146c3f
Venky <venkatramanm@gmail.com>
2024-12-09 21:19:10 +0530

    Provider made unique key

--- src/main/java/in/succinct/catalog/indexer/db/model/IndexedProviderModel.java
+++ src/main/java/in/succinct/catalog/indexer/db/model/IndexedProviderModel.java
@@ -1,12 +1,14 @@
 package in.succinct.catalog.indexer.db.model;
 
 import com.venky.swf.db.annotations.column.IS_NULLABLE;
+import com.venky.swf.db.annotations.column.UNIQUE_KEY;
 import com.venky.swf.db.annotations.column.indexing.Index;
 
 public interface IndexedProviderModel extends IndexedSubscriberModel {
 
     @Index
     @IS_NULLABLE(value = false)
+    @UNIQUE_KEY
     public Long getProviderId();
     public void setProviderId(Long id);
     public Provider getProvider();
